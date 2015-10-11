5600eddf19885cf671b24334d4c87c0bc71cb070
kkloberdanz <kyle.g.kloberdanz@gmail.com>
2015-10-11 18:16:26 -0500

    Create fixScreenResolution.sh

---
+++ fixScreenResolution.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+xrandr | grep maximum
+
+gtf 1440 900 59.9
+
+xrandr --newmode "1440x900_59.90"  106.29  1440 1520 1672 1904  900 901 904 932  -HSync +Vsync
+
+xrandr --addmode VGA-0 1440x900_59.90
+
+xrandr --output VGA-0 --mode 1440x900_59.90
