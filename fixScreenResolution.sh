65ba21c9010141d4169c2748c6667993065b100a
kkloberdanz <kyle.g.kloberdanz@gmail.com>
2015-12-02 11:15:20 -0600

    New graphics card, so changed VGA to DVI

--- fixScreenResolution.sh
+++ fixScreenResolution.sh
@@ -6,6 +6,6 @@ gtf 1440 900 59.9
 
 xrandr --newmode "1440x900_59.90"  106.29  1440 1520 1672 1904  900 901 904 932  -HSync +Vsync
 
-xrandr --addmode VGA-0 1440x900_59.90
+xrandr --addmode DVI-0 1440x900_59.90
 
-xrandr --output VGA-0 --mode 1440x900_59.90
+xrandr --output DVI-0 --mode 1440x900_59.90
