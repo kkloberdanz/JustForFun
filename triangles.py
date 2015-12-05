35d3782aa1126a922d177df49df703573011254a
kkloberdanz <kyle.g.kloberdanz@gmail.com>
2015-12-04 22:32:08 -0600

    Create triangles.py
    A fun thing to display on the screen when you want to look like a hacker

---
+++ triangles.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+
+# Programmer   :  Kyle Kloberdanz
+# Date Created :  08 APR 2015
+# Description  :  This program will create a wave form when run.
+#                 To run on linux: open the terminal,
+#                 then type 'python3 triangles.py'    
+
+import time
+from time import sleep
+import shutil
+
+
+x = 1
+DELAY = 0.005 # seconds
+while x < 10000:
+    width, height = shutil.get_terminal_size()
+    SIZE = width # maximum length of stars
+
+    j = 1
+    i = 1
+    time.sleep(DELAY)
+    while i < SIZE:
+        time.sleep(DELAY)
+        while j < i:        
+                time.sleep(DELAY)
+                print(j * "*")
+                j += 1
+
+        while j > 0:
+                time.sleep(DELAY)
+                print(j * "*")
+                j -= 1
+        i += 1
+
+    while i > 0:
+        time.sleep(DELAY)
+        while j < i:        
+                time.sleep(DELAY)
+                print(j * "*")
+                j += 1
+
+        while j > 0:
+                time.sleep(DELAY)
+                print(j * "*")
+                j -= 1
+
+        i -= 1
+    
+    x += 1 
